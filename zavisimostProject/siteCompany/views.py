63b859f68fb7700f505889ceaf694c767c729cd8
DrizzllDev <drizzllsvery@yandex.ru>
2023-01-31 14:08:13 +0300

    обновил отзывы

--- zavisimostProject/siteCompany/views.py
+++ zavisimostProject/siteCompany/views.py
@@ -16,12 +16,12 @@ def index(request):
             r = requests.get(f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={data}')
         except Exception as e:
             data = f'Пришел новый лид с сайта!\nИмя: {request.POST.get("name", "ошибка")}\nТелефон: {request.POST.get("phone", "ошибка")}'
-            r = requests.get(
+            req = requests.get(
                 f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={data}')
             error = f'На сайте алкашей произошла ошибка!\n{e}'
             r = requests.get(
                 f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=272635960&text={error}')
-    return render(request, 'siteCompany/index/index.html',{'utm_campaign':request.GET.get('utm_campaign','нема')})
+    return render(request, 'siteCompany/index/index.html',{'utm_campaign':request.GET.get('utm_campaign',' ')})
 
 def thsPage(request):
     return render(request, 'siteCompany/index/ths.html')
