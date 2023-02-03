9429ed0181f111b297d0ac460b5ac50d78e93ebe
Drizzlls <drizzlls@yandex.ru>
2023-02-03 19:18:07 +0300

    добавил политику конфинециаольности

--- zavisimostProject/siteCompany/urls.py
+++ zavisimostProject/siteCompany/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index, thsPage
+from .views import index, thsPage, privacy
 
 urlpatterns = [
     path('', index,name='index'),
-    path('tu_page/', thsPage, name='ths')
+    path('tu_page/', thsPage, name='ths'),
+    path('privacy-policy/', privacy, name='privacy')
 ]
--- zavisimostProject/siteCompany/views.py
+++ zavisimostProject/siteCompany/views.py
@@ -12,14 +12,15 @@ def index(request):
                 utm_campaign=request.POST['utm_campaign']
             )
             data = f'Пришел новый лид с сайта!\nИмя: {request.POST.get("name","ошибка")}\nТелефон: {request.POST.get("phone","ошибка")}'
-
-            r = requests.get(f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={data}')
+            dataVlad = f'Пришел новый лид с сайта!\nИмя: {request.POST.get("name","ошибка")}\nТелефон: {request.POST.get("phone","ошибка")}\nutm_campaign={request.POST.get("utm_campaign","ошибка")}'
+            r = requests.get(f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={dataVlad}')
             r = requests.get(f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=5901410161&text={data}')
             return redirect('ths')
         except Exception as e:
+            dataVlad = f'Пришел новый лид с сайта!\nИмя: {request.POST.get("name","ошибка")}\nТелефон: {request.POST.get("phone","ошибка")}\nutm_campaign: {request.POST.get("utm_campaign","ошибка")}'
             data = f'Пришел новый лид с сайта!\nИмя: {request.POST.get("name", "ошибка")}\nТелефон: {request.POST.get("phone", "ошибка")}'
             req = requests.get(
-                f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={data}')
+                f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=428552889&text={dataVlad}')
             error = f'На сайте алкашей произошла ошибка!\n{e}'
             r = requests.get(
                 f'https://api.telegram.org/bot5997431556:AAHstK-PgpAGwp31A4xJirfO4QWRNatGDmk/sendMessage?chat_id=272635960&text={error}')
@@ -29,3 +30,5 @@ def index(request):
 def thsPage(request):
     return render(request, 'siteCompany/index/ths.html')
 
+def privacy(request):
+    return render(request, 'siteCompany/index/privacy.html')
